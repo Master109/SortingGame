8d46ef157803e6b05bb02d4ab6d3d79560046ecb
Master109 <gileadcosman@Gileads-MacBook-Pro.local>
2014-08-02 15:05:33 -0700

    Dragging squares

--- SotringGame.spritebuilder/Source/MainScene.m
+++ SotringGame.spritebuilder/Source/MainScene.m
@@ -8,12 +8,15 @@
 
 #import "MainScene.h"
 #import "Global.h"
+#import "Square.h"
 
 @implementation MainScene
 static MainScene* _sharedMySingleton = nil;
 CGPoint _pTouchLoc;
 CGPoint _moveVec;
 Global* global;
+NSArray* squares;
+Square* draggingSquare;
 
 +(MainScene*)sharedMySingleton
 {
@@ -54,23 +57,31 @@ Global* global;
     
 -(void)update:(CCTime)delta
 {
-    
+    if (draggingSquare != nil)
+    {
+        s.position = [global VectorAdd:s.position to:_moveVec];
+    }
 }
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
+    for  (Square* s in squares)
+    {
+        if ([global Distance:s.position to:[touch locationInNode:self]] < 10)
+            draggingSquare = s;
+    }
     _pTouchLoc = [touch locationInNode:self];
 }
 
 - (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
 {
-    _moveVec = [global VectorSub:[touch locationInNode:self] from:_pTouchLoc];
+    _moveVec = [global VectorMultFloat:[global VectorSub:[touch locationInNode:self] from:_pTouchLoc] by:60];
     _pTouchLoc = [touch locationInNode:self];
 }
 
 - (void)touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
 {
-    
+    draggingSquare = nil;
 }
 
 @end
