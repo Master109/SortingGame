091998c38fc92e1dfe9e7eecaa9c9bf4f569f12f
YAHACODE <yahya@yahyas-MacBook-Air.local>
2014-08-02 14:36:00 -0700

    startscene

---
+++ SotringGame.spritebuilder/Source/StartScene.h
@@ -0,0 +1,13 @@
+//
+//  StartScene.h
+//  SotringGame
+//
+//  Created by yahya on 8/2/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCScene.h"
+
+@interface StartScene : CCScene
+
+@end
